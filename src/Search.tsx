cdb3d49e5fcc886d8f1ff51e47da5371699c33aa
chompooratta <chompoorattapornrat@gmail.com>
2025-06-17 01:26:01 +0700

    Update : Edit Search

--- src/Search.tsx
+++ src/Search.tsx
@@ -1,13 +1,19 @@
 
+import React from 'react';
 import './Search.css';
+import * as FaIcons from 'react-icons/fa';
+
 
 export default function Search () {
   return (
     <div id="search">
       <div id="search-box">
-        <input placeholder="Type something..." />
-        <button>Search</button>
+        <input type='text' className='input' placeholder="Type something..." />
+        <button className='btn'>
+          {React.createElement(FaIcons.FaSearch as React.ElementType)}
+        </button>
       </div>
     </div>
   )
 }
+
